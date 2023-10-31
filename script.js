cb8fb5eb24c28e28476237d9f41ade3d14e5a3d9
gitArt57 <139042826+gitArt57@users.noreply.github.com>
2023-10-31 20:18:05 +0500

    Add files via upload
    playable version without prompt checking

--- script.js
+++ script.js
@@ -2,72 +2,54 @@ let computerChoice = '';
 let personChoice = '';
 let computerScore = 0;
 let personScore = 0;
+let gameCounter = 0;
 let gameTools = ['rock', 'paper', 'scissors'];
-let fc = function () {
-    let random = Math.floor(Math.random() * gameTools.length);
-    let randomizedTool = gameTools[random];
-    return String(randomizedTool);
-}
-// fc(gameTools);
-console.log(computerChoice);
-console.log(personChoice);
-computerChoice = fc(gameTools);
-console.log(computerChoice);
-let a = prompt('rock, paper or scisscors', )
-personChoice = a;
-console.log(personChoice);
 
-let oneRounded = (personChoice, computerChoice) => {
+function getComputerChoice() {
+        let random = Math.floor(Math.random() * gameTools.length);
+        let randomizedTool = gameTools[random];
+        computerChoice = randomizedTool
+}
+
+function getUserChoice() {
+    let a = prompt('rock, paper or scisscors', )
+    personChoice = a;
+}
+
+let oneRounded = () => {
+    gameCounter++;
+    getUserChoice();
+    getComputerChoice();
     if (personChoice == computerChoice) {
         console.log('TIE');
     }
     if (personChoice !== computerChoice) {
         if (personChoice == 'rock' && computerChoice == 'paper') {
-            computerScore = computerScore++
+            computerScore++
         }
         if (personChoice == 'rock' && computerChoice == 'scissors') {
-            personScore = personScore++
+            personScore++
         }
         if (personChoice == 'paper' && computerChoice == 'rock') {
-             personScore = personScore++
+             personScore++
         }
         if (personChoice == 'paper' && computerChoice == 'scissors') {
-             computerScore = computerScore++
+             computerScore++
         }
         if (personChoice == 'scissors' && computerChoice == 'paper') {
-             personScore = personScore++
+             personScore++
         }
         if (personChoice == 'scissors' && computerChoice == 'rock') {
-             computerScore = computerScore++
+            computerScore++
         }
         console.log('game plays right');
     }
+    if (gameCounter == 6 || personScore == 3 || computerScore == 3) {
+        return
+    }
+    oneRounded();
 }
 oneRounded(personChoice, computerChoice);
 console.log(personScore);
 console.log(computerScore);
-
-
-
-
-
-// let personChoice = 'paper'
-// console.log(typeof computerChoice)
-// console.log(typeof personChoice)
-// if(computerChoice.toString == personChoice){
-//     console.log('ya genii')
-// }
-// else {
-//     console.log('ya loh asdsda')
-// getComputerChoice()
-// function theGame (computerChoice, playerSelection) {
-//     if (computerChoice == playerSelection) {
-//         console.log('hello world')
-//     }
-//     else {
-//         console.log('ti kamen')
-//     }
-// }
-// let playerSelection = 'rock';
-
-// theGame();
+console.log('games played = ' + gameCounter);
